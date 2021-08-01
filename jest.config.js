1c22871930590844390f4f23703c6fcf2f547f5a
scava <sorzesimone1@gmail.com>
2021-08-01 21:54:06 +0200

    fix

--- jest.config.js
+++ jest.config.js
@@ -1,26 +1,25 @@
-const {defaults: tsjPreset} = require('ts-jest/presets');
+const { defaults: tsjPreset } = require('ts-jest/presets');
 
 module.exports = {
-  ...tsjPreset,
-  preset: 'react-native',
-  globals: {
-    'ts-jest': {
-      babelConfig: true,
-    },
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
-    '^~/(.*)$': '<rootDir>/$1',
-    '\\.svg': '<rootDir>/__mocks__/svgMock.js',
-    'react-native-code-push': '<rootDir>/__mocks__/react-native-code-push.js',
-    '@react-navigation': '<rootDir>/__mocks__/@react-navigation.js',
-  },
-  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-  transformIgnorePatterns: [
-    '<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|react-native-code-push)',
-  ],
-  testPathIgnorePatterns: ['<rootDir>/node_modules/', '\\.snap$'],
-  cacheDirectory: '.jest/cache',
-  testEnvironment: 'jsdom',
-  setupFiles: ['<rootDir>/__mocks__/mock-setup.js'],
+	...tsjPreset,
+	preset: 'react-native',
+	globals: {
+		'ts-jest': {
+			babelConfig: true,
+		},
+	},
+	moduleNameMapper: {
+		'^@/(.*)$': '<rootDir>/$1',
+		'^~/(.*)$': '<rootDir>/$1',
+		'\\.svg': '<rootDir>/__mocks__/svgMock.js',
+		'react-native-code-push': '<rootDir>/__mocks__/react-native-code-push.js',
+		'@react-navigation': '<rootDir>/__mocks__/@react-navigation.js',
+	},
+	testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+	transformIgnorePatterns: [
+		'<rootDir>/node_modules/(react-clone-referenced-element|@react-native-community|react-navigation|@react-navigation/.*|@unimodules/.*|native-base|react-native-code-push)',
+	],
+	testPathIgnorePatterns: ['<rootDir>/node_modules/', '\\.snap$'],
+	cacheDirectory: '.jest/cache',
+	testEnvironment: 'jsdom',
 };
